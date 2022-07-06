55cb0f4711b8595880d5884420bef97d69be00c4
Jacob Knudson <jacoblknudson@gmail.com>
2022-07-06 17:22:33 -0600

    84% done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
+import Setup from "./tutorial/8-useContext/setup/1-context-api";
 
 function App() {
   return (
