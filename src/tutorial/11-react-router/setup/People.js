b6dc34a489630e66a74397326d5af03aad54c1c2
Jacob Knudson <jacoblknudson@gmail.com>
2022-07-07 14:40:44 -0600

    finshed tutorial

--- src/tutorial/11-react-router/setup/People.js
+++ src/tutorial/11-react-router/setup/People.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { data } from '../../../data';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { data } from "../../../data";
+import { Link } from "react-router-dom";
 const People = () => {
   const [people, setPeople] = useState(data);
   return (
@@ -8,9 +8,9 @@ const People = () => {
       <h1>People Page</h1>
       {people.map((person) => {
         return (
-          <div key={person.id} className='item'>
+          <Link key={person.id} className="item" to={`/person/${person.id}`}>
             <h4>{person.name}</h4>
-          </div>
+          </Link>
         );
       })}
     </div>
